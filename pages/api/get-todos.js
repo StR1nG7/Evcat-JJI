a03924aace32750733f9bf07dd0f753d2ef84066
unknown <demomusicevcat@gmail.com>
2022-12-27 20:09:29 +0100

    deploy

--- pages/api/get-todos.js
+++ pages/api/get-todos.js
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-    if (req.key === process.env.NEXT_PUBLIC_TODO_KEY) {
+    if (req.key !== process.env.NEXT_PUBLIC_TODO_KEY) {
         res.send("Invalid key")
     }
     try {
